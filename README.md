101d31e373c4921f4841d2580e65eca64777a4d1
evaldowolkers <evaldowolkers@gmail.com>
2021-08-31 09:48:09 -0300

    Subindo arquivos do projeto

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # study-python-microservices-01
 Repositório de estudo de microsserviços em Python (01).
+
+
+# Execução
+Acesse a pasta app e execute:
+uvicorn main:app --reload
+
+Acesse a porta 3000 pelo navegador e veja o resultado.
+
+# Fonte
+https://digitalinnovation.one/artigos/python-in-microservice
---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+fake_movie_db = [
+    {
+        'name': 'Star Wars: Episode IX - The Rise of Skywalker',
+        'plot': 'The surviving members of the resistance face the First Order once again.',
+        'genres': ['Action', 'Adventure', 'Fantasy'],
+        'casts': ['Daisy Ridley', 'Adam Driver']
+    }
+]
+
+class Movie(BaseModel):
+    name: str
+    plot: str
+    genres: List[str]
+    casts: List[str]
+
+@app.get('/', response_model=List[Movie])
+async def index():
+    return fake_movie_db
